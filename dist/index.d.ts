ba35da4b00bb82da9b60bcb1dddb6316ca629e16
Travis Rodgers <travis@travis.media>
2026-07-11 22:46:58 -0400

    Sync lockfile to 1.1.1 and stop committing dist now that the package is on npm

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroIntegration } from 'astro';
-import type { LinkValidatorOptions } from './types';
-/**
- * Creates the Astro Link Validator integration
- */
-export default function linkValidator(options?: LinkValidatorOptions): AstroIntegration;
-export type { LinkValidatorOptions, Link, BrokenLink, LinkCheckResult } from './types';
-export { checkLinks, extractLinksFromHtml } from './link-checker.js';
--- dist/link-checker.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Link, LinkCheckResult, LinkValidatorOptions } from './types';
-/**
- * Extract links from HTML content using Cheerio
- */
-export declare function extractLinksFromHtml(html: string, sourceFile: string): Link[];
-/**
- * Main function to check all links in the build directory
- */
-export declare function checkLinks(buildDir: string, options?: LinkValidatorOptions): Promise<LinkCheckResult>;
